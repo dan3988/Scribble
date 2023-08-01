6bfd9959ef3fa38ce28148bcc6babd80b9e58b9e
daniel.pickett@hotmail.co.uk <40858622+dan3988@users.noreply.github.com>
2023-08-01 21:28:42 +0100

    Added shared implementation for actions that only use 2 points to draw

--- Tools/LineTool.cs
+++ Tools/LineTool.cs
@@ -2,21 +2,11 @@
 
 namespace Scribble.Tools;
 
-//public sealed class LineToolType : ScribbleToolType
-//{
-//	public static readonly LineToolType Instance = new();
-
-//	private LineToolType() : base("line", "Line")
-//	{
-//	}
-
-//	public override LineTool CreateTool()
-//		=> new();
-//}
-
 public sealed class LineTool : ScribbleTool
 {
 	public static readonly LineTool Instance = new();
+
+	private static readonly SimpleDrawHandler mDrawHandler = (canvas, paint, start, end) => canvas.DrawLine(start, end, paint);
 
 	private LineTool() : base("line", "Line")
 	{
@@ -25,46 +15,6 @@ public sealed class LineTool : ScribbleTool
 	public override IScribbleAction Begin(SKPoint point, SKColor color, int size)
 	{
 		var paint = CreateStrokePaint(color, size);
-		return new Action(paint, point);
-	}
-
-	private class Element : IScribbleElement
-	{
-		internal readonly SKPaint mPaint;
-		internal SKPoint mStart, mEnd;
-
-		public Element(SKPaint paint, SKPoint start, SKPoint end)
-		{
-			mPaint = paint;
-			mStart = start;
-			mEnd = end;
-		}
-
-		void IDisposable.Dispose()
-		{
-			mPaint.Dispose();
-		}
-
-		void IScribbleElement.Draw(SKCanvas canvas, SKImageInfo canvasInfo)
-			=> canvas.DrawLine(mStart, mEnd, mPaint);
-	}
-
-	private sealed class Action : Element, IScribbleAction
-	{
-		public Action(SKPaint paint, SKPoint start) : base(paint, start, start)
-		{
-		}
-
-		public IScribbleElement Complete(SKPoint point)
-			=> new Element(mPaint.Clone(), mStart, point);
-
-		public bool OnDrag(SKPoint point)
-		{
-			if (mEnd == point)
-				return false;
-
-			mEnd = point;
-			return true;
-		}
+		return CreateSimpleAction(mDrawHandler, paint, point);
 	}
 }
--- Tools/ScribbleTool.cs
+++ Tools/ScribbleTool.cs
@@ -4,6 +4,11 @@ namespace Scribble.Tools;
 
 public abstract class ScribbleTool
 {
+	protected delegate void SimpleDrawHandler(SKCanvas canvas, SKPaint paint, SKPoint start, SKPoint end);
+
+	protected static IScribbleAction CreateSimpleAction(SimpleDrawHandler handler, SKPaint paint, SKPoint start)
+		=> new SimpleAction(handler, paint, start);
+
 	protected static SKPaint CreateStrokePaint(SKColor color, int size) => new()
 	{
 		Color = color,
@@ -32,4 +37,47 @@ public abstract class ScribbleTool
 	}
 
 	public abstract IScribbleAction Begin(SKPoint point, SKColor color, int size);
+
+
+	private class SimpleElement : IScribbleElement
+	{
+		internal readonly SimpleDrawHandler mHandler;
+		internal readonly SKPaint mPaint;
+		internal SKPoint mStart, mEnd;
+
+		public SimpleElement(SimpleDrawHandler handler, SKPaint paint, SKPoint start, SKPoint end)
+		{
+			mHandler = handler;
+			mPaint = paint;
+			mStart = start;
+			mEnd = end;
+		}
+
+		void IDisposable.Dispose()
+		{
+			mPaint.Dispose();
+		}
+
+		void IScribbleElement.Draw(SKCanvas canvas, SKImageInfo canvasInfo)
+			=> mHandler.Invoke(canvas, mPaint, mStart, mEnd);
+	}
+
+	private sealed class SimpleAction : SimpleElement, IScribbleAction
+	{
+		public SimpleAction(SimpleDrawHandler handler, SKPaint paint, SKPoint start) : base(handler, paint, start, start)
+		{
+		}
+
+		public IScribbleElement Complete(SKPoint point)
+			=> new SimpleElement(mHandler, mPaint.Clone(), mStart, point);
+
+		public bool OnDrag(SKPoint point)
+		{
+			if (mEnd == point)
+				return false;
+
+			mEnd = point;
+			return true;
+		}
+	}
 }
